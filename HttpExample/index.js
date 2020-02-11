a0bead2f18f84dc6e920df13b21c352816fc6b13
Arjun Attam <arjunattam@gmail.com>
2020-02-11 15:17:30 -0800

    Working example for playwright-chromium

---
+++ HttpExample/index.js
@@ -0,0 +1,24 @@
+//@ts-check
+const chromium = require("playwright-chromium");
+const fs = require("fs");
+
+module.exports = async function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+  const browser = await chromium.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+  const browserContext = await browser.newContext();
+  const page = await browserContext.newPage();
+  await page.goto("https://google.com");
+  await page.screenshot({ path: "google.png" });
+  await browser.close();
+
+  context.res = {
+    status: 202,
+    body: Buffer.from(fs.readFileSync("google.png")),
+    headers: {
+      "Content-Type": "image",
+      "Content-Disposition": `attachment; filename=google.png`
+    }
+  };
+};
